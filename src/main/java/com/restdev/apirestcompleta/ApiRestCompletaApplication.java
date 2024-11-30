6b4d499ba20d19949f551c2d71e6711bf9483ac1
Adriley francisco almeida pereira <139196116+adrileyf-dev@users.noreply.github.com>
2024-11-30 00:15:46 -0300

    inicio de Projeto
    Desenvolvimento de uma api de estacionamnto

---
+++ src/main/java/com/restdev/apirestcompleta/ApiRestCompletaApplication.java
@@ -0,0 +1,13 @@
+package com.restdev.apirestcompleta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestCompletaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestCompletaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restdev/apirestcompleta/ApiRestCompletaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restdev.apirestcompleta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCompletaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
