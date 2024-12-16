1ccc589f28183473c63efb5374ccfc2c1e0ceb55
Adriley francisco almeida pereira <139196116+adrileyf-dev@users.noreply.github.com>
2024-12-16 16:29:30 -0300

    Ajuste de paginaçaõ 02

--- src/main/java/com/restdev/apirestcompleta/web/controller/ClienteController.java
+++ src/main/java/com/restdev/apirestcompleta/web/controller/ClienteController.java
@@ -21,6 +21,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -81,7 +82,7 @@ public class ClienteController {
     }
     @GetMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<PageableDto> getAll(Pageable  pageable) {
+    public ResponseEntity<PageableDto> getAll(@PageableDefault(size = 5,sort = {"nome"})  Pageable  pageable) {
         Page<ClienteProjection> clientes = clienteService.buscarTodos(pageable);
         return ResponseEntity.ok(pageMapper.ToDto(clientes));
     }
